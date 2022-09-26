560334d3112358466776d9b4f7c25b3fe739dede
oguzzx <1204225052@nku.edu.tr>
2022-09-26 15:13:32 +0300

    yazi_tura_app
    yazı tura app

---
+++ yazı-tura/app/src/CoinContainer.js
@@ -0,0 +1,63 @@
+import React, { Component } from "react";
+import { choice } from "./Helpers";
+import Coin from "./Coin";
+import "./coinContainer.css";
+
+class CoinContainer extends Component {
+  static defaultProps = {
+    coins: [
+      {
+        side: "tails",
+        imgSrc:
+          "https://upload.wikimedia.org/wikipedia/commons/6/64/1TL_obverse.png",
+      },
+      {
+        side: "heads",
+        imgSrc:
+          "https://upload.wikimedia.org/wikipedia/commons/c/cd/1TL_reverse.png",
+      },
+    ],
+  };
+  constructor(props) {
+    super(props);
+    this.state = {
+      currCoin: null,
+      nFlips: 0,
+      nHeads: 0,
+      nTails: 0,
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  flipCoin() {
+    const newCoin = choice(this.props.coins);
+    this.setState((st) => {
+      return {
+        currCoin: newCoin,
+        nFlips: st.nFlips + 1,
+        nHeads: st.nHeads + (newCoin.side === "heads" ? 1 : 0),
+        nTails: st.nTails + (newCoin.side === "tails" ? 1 : 0),
+      };
+    });
+  }
+
+  handleClick(e) {
+    this.flipCoin();
+  }
+  render() {
+    return (
+      <div className="CoinContainer">
+        <h1>Madeni Para Oyunu</h1>
+        {this.state.currCoin && <Coin info={this.state.currCoin} />}
+        <button onClick={this.handleClick}>Fırlat!</button>
+        <p>
+          Yapılan toplam fırlatma {this.state.nFlips}, bunların
+          {this.state.nTails} tanesi yazı ve {this.state.nHeads} tanesi de
+          turadır.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default CoinContainer;
